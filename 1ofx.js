eb7b864fe456e50c839520b8a6fb33dba1ca2bc3
Ryan Allred <ryanallredxyz@gmail.com>
2021-12-26 13:48:06 -0700

    Add tokenIndex

--- 1ofx.js
+++ 1ofx.js
@@ -3,20 +3,24 @@
  */
 
 const params = new URLSearchParams(window.location.hash.slice(1));
+const realRandom = Math.random.bind(Math);
+const tokenId = params.get("tokenId") || (realRandom() * 0xffffffff).toFixed(0);
 
-window.OneOfX = {
-  realRandom: Math.random.bind(Math),
-  tokenId: params.get("tokenId") || (Math.random() * 0xffffffff).toFixed(0),
-  save: function () {},
-};
-
-function mulberry32(a) {
+Math.random = (function mulberry32(a) {
   return function () {
     let t = (a += 0x6d2b79f5);
     t = Math.imul(t ^ (t >>> 15), t | 1);
     t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
     return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
   };
-}
+})(parseInt(tokenId.slice(2), 16));
 
-Math.random = mulberry32(parseInt(OneOfX.tokenId.slice(2), 16));
+window.OneOfX = {
+  realRandom,
+  tokenId,
+  tokenIndex: params.get("tokenIndex") || (Math.random() * 100).toFixed(0),
+  save: function (props) {
+    console.log("Properties:", props);
+    this.save = () => {};
+  },
+};
--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const height = 200;
 
 // Should your art scale to contain or cover the viewport?
 // see contain & cover: https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit
-const shouldContain = true;
+const shouldContain = false;
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const dpi = window.devicePixelRatio || 1;
+
+// requestAnimationFrame uses the display frequency which isn't always 60hz
+// always a good idea to throttle it down to your expected fps.
+const fps = 60;
 
 const scale = (
   shouldContain
@@ -27,28 +31,43 @@ canvas.height = realHeight * dpi;
 canvas.style.width = realWidth + "px";
 canvas.style.height = realHeight + "px";
 
-function loop() {
+const hue = Math.round(Math.random() * 18) * 20;
+const backgroundColor = `hsl(${hue}, 30%, 50%)`;
+const strokeStyle = `hsl(${hue + 20}, 50%, 60%)`;
+
+// 1ofx will use your background color to generate a theme
+// for your token.
+document.body.style.backgroundColor = backgroundColor;
+
+let frame = 0;
+let lastFrame = performance.now();
+(function loop() {
+  window.requestAnimationFrame(loop);
+  if (performance.now() - lastFrame < 1000 / fps) return;
+  lastFrame = performance.now();
+
+  frame++;
   canvas.width = canvas.width;
 
-  ctx.save();
+  ctx.strokeStyle = strokeStyle;
   ctx.scale(scale * dpi, scale * dpi);
 
   for (let i = 40; i < width; i += 10) {
+    ctx.lineWidth = Math.max(
+      1,
+      4 * (Math.cos(((i + -frame / 3) / width) * Math.PI * 2) + 1)
+    );
     ctx.beginPath();
     ctx.arc(width / 2, height / 2, i, 0, 2 * Math.PI);
     ctx.stroke();
   }
 
-  ctx.restore();
-
   // Once you have drawn you can
   // save any features. This will
   // also trigger your snapshot.
   window.OneOfX.save({
-    "Feature Name": 1,
+    Hue: hue,
+    "Fill Style": backgroundColor,
+    "Stroke Style": strokeStyle,
   });
-
-  window.requestAnimationFrame(loop);
-}
-
-loop();
+})();

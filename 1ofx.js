9c08dae9247a83df408fe090445cc6e1a891e2b7
Ryan Allred <ryanallredxyz@gmail.com>
2022-02-01 20:20:10 -0700

    Update 1ofx.js

--- 1ofx.js
+++ 1ofx.js
@@ -1,30 +1,28 @@
-/**
- * Don't edit this file.
- */
+if (!window.OneOfX) {
+  const params = new URLSearchParams(window.location.hash.slice(1));
+  const realRandom = Math.random.bind(Math);
+  const tokenId = params.get("tokenId") || (realRandom() * 0xffffffff).toFixed(0);
 
-const params = new URLSearchParams(window.location.hash.slice(1));
-const realRandom = Math.random.bind(Math);
-const tokenId = params.get("tokenId") || (realRandom() * 0xffffffff).toFixed(0);
+  Math.random = (function mulberry32(a) {
+    return function () {
+      let t = (a += 0x6d2b79f5);
+      t = Math.imul(t ^ (t >>> 15), t | 1);
+      t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+      return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+    };
+  })(parseInt(tokenId.slice(2), 16));
 
-Math.random = (function mulberry32(a) {
-  return function () {
-    let t = (a += 0x6d2b79f5);
-    t = Math.imul(t ^ (t >>> 15), t | 1);
-    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
-    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  window.OneOfX = {
+    realRandom,
+    tokenId,
+    tokenIndex: params.get("tokenIndex") || (Math.random() * 100).toFixed(0),
+    features: null,
+    saved: false,
+    save: function (props) {
+      if (props) this.features = props;
+      this.saved = true;
+      console.log("Properties:", props);
+      this.save = () => {};
+    },
   };
-})(parseInt(tokenId.slice(2), 16));
-
-window.OneOfX = {
-  realRandom,
-  tokenId,
-  tokenIndex: params.get("tokenIndex") || (Math.random() * 100).toFixed(0),
-  features: null,
-  saved: false,
-  save: function (props) {
-    if (props) this.features = props;
-    this.saved = true;
-    console.log("Properties:", props);
-    this.save = () => {};
-  },
-};
+}
